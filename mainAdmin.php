f7578fb461b2ecd343e4116e3e37dd61b91252b3
Adhwa Salemi <91861353+AdhyyH@users.noreply.github.com>
2023-06-17 18:33:20 +0800

    6th changes

--- mainAdmin.php
+++ mainAdmin.php
@@ -235,7 +235,9 @@
                                     </div>
                                 </form>
 
-                                
+                             <!--Count ID--->
+                             <br><br>
+
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
