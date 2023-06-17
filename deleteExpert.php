78491dbc32f958d7702d94df9fb11abee9f8b525
MNeeshallini <102916192+MNeeshallini@users.noreply.github.com>
2023-06-17 17:59:19 +0800

    Add files via upload

---
+++ deleteExpert.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fkedusearch";
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the ID of the record to delete
+    $ExpertId = $_POST['ExpertID'];
+
+    // Create a new connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare the delete statement
+    $stmt = $conn->prepare("DELETE FROM expert WHERE ExpertID = ?");
+    $stmt->bind_param("s", $ExpertId);
+
+    // Execute the delete statement
+    if ($stmt->execute()) {
+        echo "<script>alert('Data deleted successfully.'); window.location.href = 'mainAdmin.php';</script>";
+    } else {
+        echo "<script>alert('Error deleting data: " . $stmt->error . "');</script>";
+    }
+
+    // Close the statement and connection
+    $stmt->close();
+    $conn->close();
+}
+?>
